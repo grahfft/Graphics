9870edbe0206e490647d6f735b548c1dddf04322
Dennis Lassell <dennis.lassell@gmail.com>
2018-01-26 13:45:39 -0500

    Cleaned up Seirpinski and Triangle; Recognized Pattern and have implemnted; Experimented with dynamic arrays again, no luck

--- Homework1/Graphics_Starting_Code/GLSLExperiment/example1.cpp
+++ Homework1/Graphics_Starting_Code/GLSLExperiment/example1.cpp
@@ -11,12 +11,36 @@ int NumPoints = 0;
 // Function to draw
 void(*drawImage)(void);
 
-// remember to prototype
-void generateGeometry( void );
+// Triangle Proto-type TODO: move into C++ object
+void TriangleGeometry(void);
+void TriangleInitBuffers(void);
+void TriangleShaders(void);
+void TriangleDrawImage();
+void GenerateTriangle(void);
+
+// Seirpinski Proto-type TODO: move into C++ object
+void SeirpinskiShader(void);
+void SeirpinskiGeometry(void);
+void SeirpinskiInitBuffers(void);
+void SeirpinskiDrawImage(void);
 void GenerateSeirpinski(void);
-void GenerateDatFile(void);
+
+// Generate Data File Prototyping
+void DataFileShader(void);
+void DataFileGeometry(void);
+void DataFileInitBuffers(void);
+void DataFileDrawImage(void);
+void GenerateDataFile(void);
+
+// General Functions
 void initGPUBuffers( void );
-void shaderSetup( void );
+
+// Initialization prototypes
+void InitWindow(int argc, char **argv);
+void InitCallbacks(void);
+void InitShader(void);
+
+// Callbacks
 void display( void );
 void keyboard( unsigned char key, int x, int y );
 
@@ -32,9 +56,8 @@ GLuint program;
 GLenum mode = GL_LINE_LOOP;
 /* -------------------------------------------------------------- */
 
-
-/* Geometry Generation Section */
-void generateGeometry( void )
+/* Generate Triangle */
+void TriangleGeometry( void )
 {
 	// Specifiy the vertices for a triangle
 	NumPoints = 3;
@@ -44,22 +67,57 @@ void generateGeometry( void )
 	points[2] = point2( 0.5, -0.5 );
 }
 
-void GenerateSeirpinski(void)
+void TriangleInitBuffers(void)
 {
-	/*
-	Author's Notes:
-		- Requires GL_POINTS to correctly draw
-		- Drew 5000 iterations (NumPoints = 5000)
-		- Re-read chapter 2
-		- swap bug caused by not passing points into framebuffer; LINE_LOOP Test
-	*/
+	mode = GL_LINE_LOOP;
+	initGPUBuffers();
+}
 
+void TriangleShaders(void)
+{
+	// Initialize the vertex position attribute from the vertex shader
+	GLuint loc = glGetAttribLocation(program, "vPosition");
+	glEnableVertexAttribArray(loc);
+	glVertexAttribPointer(loc, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+	mat4 ortho = Ortho2D(-1.0, 1.0, -1.0, 1.0);
+	GLuint ProjLoc = glGetUniformLocation(program, "Proj");
+	glUniformMatrix4fv(ProjLoc, 1, GL_TRUE, ortho);
+}
+
+void TriangleDrawImage()
+{
+	glClear(GL_COLOR_BUFFER_BIT);                // clear window
+	glDrawArrays(mode, 0, NumPoints);    // draw the points
+	glFlush();
+}
+
+void GenerateTriangle(void)
+{
+	TriangleGeometry();
+	TriangleInitBuffers();
+	TriangleShaders();
+	TriangleDrawImage();
+}
+
+/* -------------------------------------------------------------- */
+
+/* Seirpinski Section */
+
+void SeirpinskiShader(void) 
+{
 	mat4 ortho = Ortho2D(-2.0, 2.0, -2.0, 2.0);
 	GLuint ProjLoc = glGetUniformLocation(program, "Proj");
 	glUniformMatrix4fv(ProjLoc, 1, GL_TRUE, ortho);
 
-	mode = GL_POINTS;
+	// Initialize the vertex position attribute from the vertex shader
+	GLuint loc = glGetAttribLocation(program, "vPosition");
+	glEnableVertexAttribArray(loc);
+	glVertexAttribPointer(loc, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+}
 
+void SeirpinskiGeometry(void)
+{
 	NumPoints = 10000;
 	point2 vertices[3];
 	vertices[0] = point2(-1.0, -1.0);
@@ -73,18 +131,61 @@ void GenerateSeirpinski(void)
 		int determineVertex = rand() % 3;
 		points[index] = (points[index - 1] + vertices[determineVertex]) / 2.0;
 	}
-
-	initGPUBuffers();
-	shaderSetup();
 }
 
-/*
-Author-s Notes:
-	- Adding Proj * vPosition causes other drawings to break; why?; Default Proj needed
-	- 
-*/
+void SeirpinskiInitBuffers(void)
+{
+	mode = GL_POINTS;
+	initGPUBuffers();
+}
 
-void GenerateDatFile(void)
+void SeirpinskiDrawImage(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);                // clear window
+	glDrawArrays(mode, 0, NumPoints);    // draw the points
+	glFlush();
+}
+
+void GenerateSeirpinski(void)
+{
+	/*
+	Author's Notes:
+		- Requires GL_POINTS to correctly draw
+		- Drew 5000 iterations (NumPoints = 5000)
+		- Re-read chapter 2
+		- swap bug caused by not passing points into framebuffer; LINE_LOOP Test
+		- default loc and proj are needed; best represented by a section each per 
+	*/
+
+	SeirpinskiGeometry();
+	SeirpinskiInitBuffers();
+	SeirpinskiShader();
+	SeirpinskiDrawImage();
+}
+
+/* Generate Homegrown Image File */
+
+void DataFileShader(void) 
+{
+
+}
+
+void DataFileGeometry(void)
+{
+
+}
+
+void DataFileInitBuffers(void)
+{
+
+}
+
+void DataFileDrawImage(void)
+{
+
+}
+
+void GenerateDataFile(void)
 {
 	//mat4 ortho = Ortho2D(left, right, bottom, top);
 	GLuint ProjLoc = glGetUniformLocation(program, "Proj");
@@ -108,25 +209,6 @@ void initGPUBuffers( void )
 }
 
 
-void shaderSetup( void )
-{
-	// Load shaders and use the resulting shader program
-    program = InitShader( "vshader1.glsl", "fshader1.glsl" );
-    glUseProgram( program );
-
-    // Initialize the vertex position attribute from the vertex shader
-    GLuint loc = glGetAttribLocation( program, "vPosition" );
-    glEnableVertexAttribArray( loc );
-    glVertexAttribPointer( loc, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0) );
-
-	mat4 ortho = Ortho2D(-1.0, 1.0, -1.0, 1.0);
-	GLuint ProjLoc = glGetUniformLocation(program, "Proj");
-	glUniformMatrix4fv(ProjLoc, 1, GL_TRUE, ortho);
-
-    glClearColor( 1.0, 1.0, 1.0, 1.0 );        // sets white as color used to clear screen
-}
-
-
 void display( void )
 {
 	// All drawing happens in display function
@@ -143,7 +225,9 @@ void keyboard( unsigned char key, int x, int y )
     switch ( key ) {
 	case 's':
 		GenerateSeirpinski();
-		display();
+		break;
+	case 't':
+		GenerateTriangle();
 		break;
     case 033:			// 033 is Escape key octal value
         exit(1);		// quit program
@@ -151,27 +235,41 @@ void keyboard( unsigned char key, int x, int y )
     }
 }
 
+void InitWindow(int argc, char **argv)
+{
+	glutInit(&argc, argv);                       // intialize GLUT  
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // single framebuffer, colors in RGB
+	glutInitWindowSize(640, 480);                // Window size: 640 wide X 480 high
+	glutInitWindowPosition(100, 150);               // Top left corner at (100, 150)
+	glutCreateWindow("Starting App");            // Create Window
+
+	glewInit();										// init glew
+}
+
+void InitShader(void)
+{
+	// Load shaders and use the resulting shader program
+	program = InitShader("vshader1.glsl", "fshader1.glsl");
+	glUseProgram(program);
+
+	glClearColor(1.0, 1.0, 1.0, 1.0);        // sets white as color used to clear screen
+}
+
+void InitCallbacks(void)
+{
+	glutDisplayFunc(display);                    // Register display callback function
+	glutKeyboardFunc(keyboard);                  // Register keyboard callback function
+}
 
 int main( int argc, char **argv )
 {
 	// main function: program starts here
+	InitWindow(argc, argv);
+	InitShader();
+	InitCallbacks();
 
-    glutInit( &argc, argv );                       // intialize GLUT  
-    glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB ); // single framebuffer, colors in RGB
-    glutInitWindowSize( 640, 480 );                // Window size: 640 wide X 480 high
-	glutInitWindowPosition(100,150);               // Top left corner at (100, 150)
-    glutCreateWindow( "Starting App" );            // Create Window
-
-    glewInit();										// init glew
-
-	GenerateDatFile();
-    generateGeometry( );                           // Call function that generates points to draw
-	//GenerateSeirpinski();
-	initGPUBuffers( );							   // Create GPU buffers
-    shaderSetup( );                                // Connect this .cpp file to shader file
-
-    glutDisplayFunc( display );                    // Register display callback function
-    glutKeyboardFunc( keyboard );                  // Register keyboard callback function
+	// GenerateDatFile();
+	GenerateTriangle();                             
 
 	// Can add minimalist menus here
 	// add mouse handler
